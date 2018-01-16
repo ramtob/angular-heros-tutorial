bb1a41893dd80676548c3fe207c65bebeda43410
ramtob <doron.maman@gmail.com>
2018-01-16 15:38:12 +0200

    Display a Heroes List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero} from "../hero";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,16 @@ import { Hero} from "../hero";
 })
 export class HeroesComponent implements OnInit {
 
-  hero = {
-    id: 1,
-    name: 'Hill'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Hill' },
+  { id: 12, name: 'Greens' },
+  { id: 13, name: 'Blues' }
+  ];
+
