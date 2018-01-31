a638db84fa846435ea17db98dbc3d1ef7a8df7e1
Ram Tobolski <ramtob@gmail.com>
2018-01-31 12:10:23 +0200

    Http (not finished)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {HeroService} from "./hero.service";
-import { MessagesService } from './messages.service';
-import { MessagesComponent } from './messages/messages.component';
+import {MessagesService} from './messages.service';
+import {MessagesComponent} from './messages/messages.component';
 import {AppRoutingModule, routes} from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HttpClientModule} from "@angular/common/http";
+import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./in-memory-data.service";
 
 
 @NgModule({
@@ -24,7 +27,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [
     HeroService, MessagesService,
